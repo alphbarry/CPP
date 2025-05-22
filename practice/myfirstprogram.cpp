f9dfa711cff006f5fee6844fba17e2c655a309aa
alphbarr <alphaomar9@gmail.com>
2025-05-22 21:40:53 +0200

    first steps

---
+++ practice/myfirstprogram.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <iostream>
+
+int	main() {
+	int	a = 5;
+	int	b = 10;
+	int	c = a + b;
+	int	x;
+	int MyAge = 25;
+	float Height = 1.80;
+	const string = "John Doe";
+	std::cout << "The sum of a: " << a << " and b: " << b << " is c:" << c << std::endl;
+	std::cout << "My name is " << name << ", I am " << MyAge << " years old, and " << Height << " feet tall." << std::endl;;
+	std::cout << "type a number: ";
+	std:: cin >> x;
+	std::cout << "You entered: " << x << std::endl;
+	return 0;
+}
