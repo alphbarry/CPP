0b084a330fa74944d7fa84ae065c60a9e541ee5b
alphbarr <alphaomar9@gmail.com>
2025-06-09 18:12:36 +0200

    ex05 cpp01 done

--- CPP-01/ex05/src/main.cpp
+++ CPP-01/ex05/src/main.cpp
@@ -6,8 +6,21 @@
 /*   By: alphbarr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/09 16:40:21 by alphbarr          #+#    #+#             */
-/*   Updated: 2025/06/09 16:40:23 by alphbarr         ###   ########.fr       */
+/*   Updated: 2025/06/09 18:12:03 by alphbarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/Harl.hpp"
 
+int	main(){
+	Harl	harl;
+
+	std::cout << GRAY << "Harl is ready to complain." << std::endl;
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+
+	std::cout << GRAY << "Harl has finished complaining." << std::endl;
+	return (0);
+}
