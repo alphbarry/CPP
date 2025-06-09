da32796212d35251d4faf865a35a67367b47f86c
alphbarr <alphaomar9@gmail.com>
2025-06-09 16:21:37 +0200

    just change the first line

--- CPP-01/ex04/main.cpp
+++ CPP-01/ex04/main.cpp
@@ -5,18 +5,42 @@
 int	main(int ac, char **av) {
 	std::string	s1;
 	std::string	s2;
-	std::string	filename;
-	std::ifstream	filename_replace;
-	std::string	chr;
+	std::string	filename_r;
+	std::ifstream	filename;
+	std::ofstream	filename_replace;
+	std::string	line;
 
 	if (ac != 4) {
 		std::cout << "Usage: " << av[0] << " <string1> <string2>" << std::endl;
 		return 1;
 	}
-	filename = av[1];
+	filename.open(av[1]);
 	s1 = av[2];
 	s2 = av[3];
-	chr = strchr(filename, s1);
-	
+	filename_r = std::string(av[1]) + ".replace";
+	filename_replace.open(filename_r.c_str());
+	if (!filename.is_open()) {
+		std::cerr << "Error opening file!" << std::endl;
+		return 1;
+	}
+	else {
+		if (!filename_replace.is_open()){
+			std::cerr << "Error opening file for writing!" << std::endl;
+			return 1;
+		}
+		while (getline(filename, line)) {
+			std::cout << "Processing line: " << line << std::endl;
+			size_t pos = 0;
+			while ((pos = line.find(s1, pos)) != std::string::npos) {
+				line.erase(pos, s1.length());
+				line.insert(pos, s2);
+				pos += s2.length();
+			}
+			filename_replace << line << std::endl;
+		}
+		filename.close();
+		filename_replace.close();
+	}
+
 	return 0;
 }
