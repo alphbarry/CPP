fa2b395ace7f7ab253c72ac6546b1d0e758b020b
alphbarry <148545202+alphbarry@users.noreply.github.com>
2025-11-19 18:02:12 +0000

    cpp6

--- CPP-06/ex02/inc/Base.hpp
+++ CPP-06/ex02/inc/Base.hpp
@@ -1,13 +1,5 @@
 #pragma once
 
-#include <iostream>
-#include <string>
-#include <iostream>
-#include <cstdlib>   // rand(), srand()
-#include <ctime>     // time()
-#include <typeinfo>  // std::bad_cast
-
-
 class Base {
 	public:
 		virtual ~Base();
--- CPP-06/ex02/src/main.cpp
+++ CPP-06/ex02/src/main.cpp
@@ -3,49 +3,54 @@
 #include "../inc/B.hpp"
 #include "../inc/C.hpp"
 
+#include <iostream>
+#include <cstdlib> // rand, srand
+#include <ctime>   // time
+#include <exception>
+
 Base* generate(void) {
 	int random = rand() % 3;
-	if (random == 0) {
+	if (random == 0)
 		return new A();
-	} else if (random == 1) {
+	if (random == 1)
 		return new B();
-	} else {
-		return new C();
-	}
+	return new C();
 }
 
 void identify(Base* p) {
-	if (dynamic_cast<A*>(p)) {
+	if (dynamic_cast<A*>(p))
 		std::cout << "A" << std::endl;
-	} else if (dynamic_cast<B*>(p)) {
+	else if (dynamic_cast<B*>(p))
 		std::cout << "B" << std::endl;
-	} else if (dynamic_cast<C*>(p)) {
+	else if (dynamic_cast<C*>(p))
 		std::cout << "C" << std::endl;
-	} else {
+	else
 		std::cout << "Unknown type" << std::endl;
-	}
 }
 
 void identify(Base& p) {
 	try {
 		A& a = dynamic_cast<A&>(p);
-		std::cout << "A" << std::endl;
 		(void)a;
-	} catch (std::bad_cast&) {
-		try {
-			B& b = dynamic_cast<B&>(p);
-			std::cout << "B" << std::endl;
-			(void)b;
-		} catch (std::bad_cast&) {
-			try {
-				C& c = dynamic_cast<C&>(p);
-				std::cout << "C" << std::endl;
-				(void)c;
-			} catch (std::bad_cast&) {
-				std::cout << "Unknown type" << std::endl;
-			}
-		}
-	}
+		std::cout << "A" << std::endl;
+		return;
+	} catch (std::exception&) {}
+
+	try {
+		B& b = dynamic_cast<B&>(p);
+		(void)b;
+		std::cout << "B" << std::endl;
+		return;
+	} catch (std::exception&) {}
+
+	try {
+		C& c = dynamic_cast<C&>(p);
+		(void)c;
+		std::cout << "C" << std::endl;
+		return;
+	} catch (std::exception&) {}
+
+	std::cout << "Unknown type" << std::endl;
 }
 
 int main() {
